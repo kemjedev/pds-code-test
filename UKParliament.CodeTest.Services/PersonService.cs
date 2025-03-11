e7dea5f17723a0c71486266b94217ec7f9aad75d
Jennifer Charles <kemjedev@outlook.com>
2025-03-11 21:09:09 +0000

    Api client features in web app consuming api endpoints Person Management UI feature components added

--- UKParliament.CodeTest.Services/PersonService.cs
+++ UKParliament.CodeTest.Services/PersonService.cs
@@ -2,7 +2,7 @@
 using System.Net.Http.Json;
 using UKParliament.CodeTest.Dtos;
 
-namespace UKParliament.CodeTest.Services;
+namespace UKParliament.CodeTest.Web.Services;
 
 public class PersonService : IPersonService
 {
@@ -18,7 +18,24 @@ public class PersonService : IPersonService
     {
         var response = await _httpClient.GetAsync("data-api/persons");
         response.EnsureSuccessStatusCode();
-        return await response.Content.ReadFromJsonAsync<IEnumerable<PersonDto>>();
+        var persons = await response.Content.ReadFromJsonAsync<IEnumerable<PersonDto>>();
+        return persons ?? Enumerable.Empty<PersonDto>();
+    }
+
+    public async Task<PersonDto> CreateNewPersonAsync(PersonDto personDto)
+    {
+        var response = await _httpClient.PostAsJsonAsync("data-api/persons", personDto);
+        response.EnsureSuccessStatusCode();
+        var createdPerson = await response.Content.ReadFromJsonAsync<PersonDto>();
+        return createdPerson ?? throw new InvalidOperationException("Failed to create person.");
+    }
+
+    public async Task<PersonDto> UpdatePersonAsync(PersonDto personDto)
+    {
+        var response = await _httpClient.PutAsJsonAsync($"data-api/persons/{personDto.Id}", personDto);
+        response.EnsureSuccessStatusCode();
+        var updatedPerson = await response.Content.ReadFromJsonAsync<PersonDto>();
+        return updatedPerson ?? throw new InvalidOperationException("Failed to update person.");
     }
 
 }
